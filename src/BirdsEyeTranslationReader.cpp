47e21e44241a4fba37b632e272cf8c0af42da394
Michał Orynicz <morynicz@gmail.com>
2013-04-30 11:15:18 +0200

    Tidy up and formatting in TangentRotationReader.

--- src/BirdsEyeTranslationReader.cpp
+++ src/BirdsEyeTranslationReader.cpp
@@ -9,14 +9,17 @@
 #include "ImageEdgeFilter.hpp"
 #include <opencv2/imgproc/imgproc.hpp>
 
+
+#include "DrawingFunctions.hpp"
 #include <iostream>
 #include <opencv2/highgui/highgui.hpp>
+
 bool horizontalPoint2Compare(cv::Point2f p1, cv::Point2f p2) {
-	return p1.x < p2.x;
+    return p1.x < p2.x;
 }
 
 bool verticalPoint2Compare(cv::Point2f p1, cv::Point2f p2) {
-	return p1.y < p2.y;
+    return p1.y < p2.y;
 }
 
 BirdsEyeTranslationReader::BirdsEyeTranslationReader(
@@ -46,47 +49,51 @@ BirdsEyeTranslationReader::~BirdsEyeTranslationReader() {
 }
 
 TranslationReader *BirdsEyeTranslationReader::constructCopy() const {
-	return new BirdsEyeTranslationReader(*this);
+    return new BirdsEyeTranslationReader(*this);
 }
 
-cv::Point3f BirdsEyeTranslationReader::readTranslation(const cv::Mat &newFrame,
-		const double& rotationAngle) {
-	cv::Mat newTransformed;
-	cv::Point3f result(0, 0, rotationAngle);
-	cv::Mat rotationMatrix = cv::getRotationMatrix2D(_rotationCenter,
-			-rotationAngle, 1);
-	int vectorHalf = 0;
-	cv::warpPerspective(newFrame, newTransformed, _homography, newFrame.size(),
-			cv::INTER_LINEAR | cv::WARP_INVERSE_MAP);
-	if (!_oldFrame.empty()) {
-		_extractor->refillFeatures(_oldFrame, _trackedFeatures, _maxFeatures);
-		_tracker->trackFeatures(_oldFrame, newTransformed, _trackedFeatures);
-		if (!_trackedFeatures.empty()) {
+cv::Point3f BirdsEyeTranslationReader::readTranslation(
+        const cv::Mat &newFrame, const double& rotationAngle) {
+    cv::Mat newTransformed;
+    cv::Point3f result(0, 0, rotationAngle);
+    cv::Mat rotationMatrix = cv::getRotationMatrix2D(_rotationCenter,
+            rotationAngle, 1);
+    int vectorHalf = 0;
+    cv::warpPerspective(newFrame, newTransformed, _homography,
+            newFrame.size(), cv::INTER_LINEAR | cv::WARP_INVERSE_MAP);
+    // imshow("c2", newTransformed);
+    //cv::waitKey(1);
+    if (!_oldFrame.empty()) {
+        _extractor->refillFeatures(_oldFrame, _trackedFeatures,
+                _maxFeatures);
+        _tracker->trackFeatures(_oldFrame, newTransformed,
+                _trackedFeatures);
+        if (!_trackedFeatures.empty()) {
 //			cv::Mat pre = drawFeatureHistory(newTransformed, _trackedFeatures);
 //			imshow("c1", pre);
 //			imshow("ol", _oldFrame);
-			for (std::list<FeatureFilter*>::iterator it = _filters.begin();
-					it != _filters.end(); ++it) {
-				_trackedFeatures = (*it)->filterFeatures(_trackedFeatures);
-			}
+            for (std::list<FeatureFilter*>::iterator it =
+                    _filters.begin(); it != _filters.end(); ++it) {
+                _trackedFeatures = (*it)->filterFeatures(
+                        _trackedFeatures);
+            }
 //			cv::Mat post = drawFeatureHistory(newTransformed, _trackedFeatures);
-//			imshow("c2", post);
-//			cv::waitKey(0);
-			std::vector<cv::Point2f> translations = computeTranslationVectors(
-					rotationMatrix);
-			vectorHalf = translations.size() / 2;
-			std::nth_element(translations.begin(),
-					translations.begin() + vectorHalf, translations.end(),
-					horizontalPoint2Compare);
-			result.x = translations[vectorHalf].x;
-			std::nth_element(translations.begin(),
-					translations.begin() + vectorHalf, translations.end(),
-					verticalPoint2Compare);
-			result.y = translations[vectorHalf].y;
-		}
-	}
-	_oldFrame = newTransformed.clone();
-	return result;
+
+            std::vector<cv::Point2f> translations =
+                    computeTranslationVectors(rotationMatrix);
+            vectorHalf = translations.size() / 2;
+            std::nth_element(translations.begin(),
+                    translations.begin() + vectorHalf,
+                    translations.end(), horizontalPoint2Compare);
+            result.x = translations[vectorHalf].x;
+            std::nth_element(translations.begin(),
+                    translations.begin() + vectorHalf,
+                    translations.end(), verticalPoint2Compare);
+            result.y = translations[vectorHalf].y;
+        }
+    }
+    _oldFrame = newTransformed.clone();
+    return result;
 }
 
 std::vector<cv::Point2f> BirdsEyeTranslationReader::computeTranslationVectors(
