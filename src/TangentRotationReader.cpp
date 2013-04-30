bb2f591f7f70581a301297ffb1fcdd2672bc3804
Michał Orynicz <morynicz@gmail.com>
2013-04-30 11:16:22 +0200

    Make TangentRotationReader work.

--- src/TangentRotationReader.cpp
+++ src/TangentRotationReader.cpp
@@ -34,7 +34,48 @@ RotationReader *TangentRotationReader::constructCopy() const {
 }
 
 float TangentRotationReader::readRotation(const cv::Mat &newFrame) {
-	float result = 0;
+    float result = 0;
+    if (!_oldFrame.empty()) {
+        _extractor->refillFeatures(_oldFrame, _trackedFeatures,
+                _maxFeatures);
+        _tracker->trackFeatures(_oldFrame, newFrame,
+                _trackedFeatures);
+        if (!_trackedFeatures.empty()) {
+            for (std::list<FeatureFilter*>::iterator it =
+                    _filters.begin(); it != _filters.end(); ++it) {
+                _trackedFeatures = (*it)->filterFeatures(
+                        _trackedFeatures);
+            }
+//            cv::Mat shown = newFrame.clone();
+//            drawFeatureHistory(shown, _trackedFeatures, 5);
+//            cv::imshow("tangent", shown);
+//            cv::waitKey(1);
+            std::vector<float> rotations = computeRotationVectors();
+            int vectorHalf = rotations.size() / 2;
+            std::nth_element(rotations.begin(),
+                    rotations.begin() + vectorHalf, rotations.end());
+            result = rotations[vectorHalf];
+        }
+    }
+    _oldFrame = newFrame.clone();
+    return result;
+}
 
-	return result;
+std::vector<float> TangentRotationReader::computeRotationVectors() {
+    std::vector<cv::Point2f> oldFeatures(_trackedFeatures.size()),
+            newFeatures(_trackedFeatures.size());
+    std::vector<float> result(oldFeatures.size());
+
+    for (unsigned int i = 0; i < result.size(); ++i) {
+        std::list<cv::Point2f>::const_iterator it =
+                _trackedFeatures[i].begin();
+        if (_trackedFeatures[i].size() > 2) {
+            result[i] = atan2(it->x - _oldFrame.cols / 2,
+                    _focalLength);
+            ++it;
+            result[i] -= atan2(it->x - _oldFrame.cols / 2, _focalLength);
+       //     std::cerr << result[i] << std::endl;
+        }
+    }
+    return result;
 }
