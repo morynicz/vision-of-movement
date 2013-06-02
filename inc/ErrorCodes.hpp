2d1642524ef58bb831989ccbc35fda1a0cf566e5
Michał Orynicz <morynicz@gmail.com>
2013-06-03 00:34:33 +0200

    Added missing file ErrorCodes.hpp to repository

---
+++ inc/ErrorCodes.hpp
@@ -0,0 +1,15 @@
+/*
+ * ErrorCodes.hpp
+ *
+ *  Created on: Apr 5, 2013
+ *      Author: link
+ */
+
+#ifndef ERRORCODES_HPP_
+#define ERRORCODES_HPP_
+
+const int USER_TRIGGERED_EXIT = 1;
+const int READ_CAMERA_PARAMETERS_FAILED = -1;
+
+
+#endif /* ERRORCODES_HPP_ */
