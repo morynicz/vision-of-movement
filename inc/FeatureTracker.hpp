5dca653178611b3a4d94c1f5b6b21930ebba90ee
Michał Orynicz <morynicz@gmail.com>
2013-07-21 19:51:24 +0200

    Added doxygen documentation to FeatureTrcker.hpp

--- inc/FeatureTracker.hpp
+++ inc/FeatureTracker.hpp
@@ -12,13 +12,25 @@
 #include <list>
 #include <opencv2/core/core.hpp>
 
+/// Base class for feature trackers
 class FeatureTracker {
-public:
-	FeatureTracker();
-	virtual FeatureTracker *constructCopy() const = 0;
-	virtual void trackFeatures(const cv::Mat &oldInput, const cv::Mat &newInput,
-			std::vector<std::list<cv::Point2f> > &trackedFeatures)=0;
-	virtual ~FeatureTracker();
+    public:
+        FeatureTracker();
+        /// Method creates a pointer to a copy of this object
+        virtual FeatureTracker *constructCopy() const = 0;
+        /// Method finds new positions of tracked features on new image frame
+
+        /**
+         * Method finds new positions of tracked features based on their past
+         * positions and current and previous frames
+         * @param oldInput - previous image frame
+         * @param newInput - current image frame
+         * @param trackedFeatures - container with tracked features
+         */
+        virtual void trackFeatures(const cv::Mat &oldInput,
+                const cv::Mat &newInput,
+                std::vector<std::list<cv::Point2f> > &trackedFeatures)=0;
+        virtual ~FeatureTracker();
 };
 
 #endif /* FEATURETRACKER_HPP_ */
