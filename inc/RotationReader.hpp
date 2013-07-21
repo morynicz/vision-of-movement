c048a46d5dde0c95c4528f0754ea49335e670fcb
Michał Orynicz <morynicz@gmail.com>
2013-07-21 20:00:21 +0200

    Added doxygen documentation to RotationReader.hpp

--- inc/RotationReader.hpp
+++ inc/RotationReader.hpp
@@ -17,19 +17,33 @@
 #include "FeatureFilter.hpp"
 #include "MovementReader.hpp"
 
-class RotationReader : public MovementReader{
-protected:
-	RotationReader(const FeatureTracker &tracker,
-	                const FeatureExtractor &extractor,
-	                const std::list<FeatureFilter*> &filters,
-	                const unsigned int &maxFeatures,
-	                const std::vector<std::list<cv::Point2f> > &trackedFeatures =
-	                        std::vector<std::list<cv::Point2f> >());
-	RotationReader(const RotationReader &toCopy);
-public:
-	virtual RotationReader *constructCopy() const=0;
-	virtual float readRotation(const cv::Mat &newFrame)=0;
-	virtual ~RotationReader();
+class RotationReader: public MovementReader {
+    protected:
+        ///Constructor
+        /**
+         * Constructor
+         * @param tracker - feature tracker
+         * @param extractor - feature extractor
+         * @param filters - list of feature filter pointers
+         * @param maxFeatures - upper limit for number of tracked features
+         * @param trackedFeatures - tracked features
+         */
+        RotationReader(const FeatureTracker &tracker,
+                const FeatureExtractor &extractor,
+                const std::list<FeatureFilter*> &filters,
+                const unsigned int &maxFeatures,
+                const std::vector<std::list<cv::Point2f> > &trackedFeatures =
+                        std::vector<std::list<cv::Point2f> >());
+        ///Deep copy constructor
+        /**
+         * Deep copy constructor
+         * @param toCopy - object to initialize with
+         */
+        RotationReader(const RotationReader &toCopy);
+    public:
+        virtual RotationReader *constructCopy() const=0;
+        virtual float readRotation(const cv::Mat &newFrame)=0;
+        virtual ~RotationReader();
 };
 
 #endif /* ROTATIONREADER_HPP_ */
