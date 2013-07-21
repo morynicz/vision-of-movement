4b0b8f181273aca9bcf4c78eec69e6277347127e
Michał Orynicz <morynicz@gmail.com>
2013-07-21 19:50:46 +0200

    Added doxygne documentation to FeatureFilter.hpp

--- inc/FeatureFilter.hpp
+++ inc/FeatureFilter.hpp
@@ -1,8 +1,8 @@
-/*
- * FeatureFilter.hpp
+/**
+ * \file FeatureFilter.hpp
  *
- *  Created on: Feb 21, 2013
- *      Author: link
+ *  \date 21.02.2013
+ *  \author Michał Orynicz
  */
 
 #ifndef FEATUREFILTER_HPP_
@@ -12,13 +12,23 @@
 #include <list>
 #include <opencv2/core/core.hpp>
 
+/// Base class for feature filters
 class FeatureFilter {
-public:
-	FeatureFilter();
-	virtual FeatureFilter *constructCopy() const = 0;
-	virtual std::vector<std::list<cv::Point2f> > filterFeatures(
-			const std::vector<std::list<cv::Point2f> > &features)=0;
-	virtual ~FeatureFilter();
+    public:
+        FeatureFilter();
+        /// Method creates a pointer to a copy of this object
+        virtual FeatureFilter *constructCopy() const = 0;
+        /// Method removes features that do not meet certain criteria
+
+        /**
+         * Method removes features that do not meet set criteria
+         * @param[in] features - features to be filtered
+         * @return container with only those features, which meet
+         * the criteria
+         */
+        virtual std::vector<std::list<cv::Point2f> > filterFeatures(
+                const std::vector<std::list<cv::Point2f> > &features)=0;
+        virtual ~FeatureFilter();
 };
 
 #endif /* FEATUREFILTER_HPP_ */
