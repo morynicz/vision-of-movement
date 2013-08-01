0228e5310b1123ea8f7ceaf567800a71546909fe
Michał Orynicz <morynicz@gmail.com>
2013-08-01 20:47:06 +0200

    Code cleanup and formatting in FeatureExtractor.cpp

--- src/FeatureExtractor.cpp
+++ src/FeatureExtractor.cpp
@@ -7,70 +7,75 @@
 #include "FeatureExtractor.hpp"
 
 bool comparePoints(cv::Point2f p1, cv::Point2f p2) {
-	if (p1.x != p2.x) {
-		return p1.x < p2.x;
-	} else {
-		return p1.y < p2.y;
-	}
-	return false;
+    if (p1.x != p2.x) {
+        return p1.x < p2.x;
+    } else {
+        return p1.y < p2.y;
+    }
+    return false;
 }
 
-bool compareListPoints(std::list<cv::Point2f> l1, std::list<cv::Point2f> l2) {
-	cv::Point2f p1 = l1.front();
-	cv::Point2f p2 = l2.front();
+bool compareListPoints(std::list<cv::Point2f> l1,
+        std::list<cv::Point2f> l2) {
+    cv::Point2f p1 = l1.front();
+    cv::Point2f p2 = l2.front();
 
-	if (p1.x != p2.x) {
-		return p1.x < p2.x;
-	} else if (p1.y != p2.y) {
-		return p1.y < p2.y;
-	} else if (l1.size() != l2.size()) {
-		return l1.size() > l2.size();
-	}
-	return false;
+    if (p1.x != p2.x) {
+        return p1.x < p2.x;
+    } else if (p1.y != p2.y) {
+        return p1.y < p2.y;
+    } else if (l1.size() != l2.size()) {
+        return l1.size() > l2.size();
+    }
+    return false;
 }
 
-bool listPointsEqual(std::list<cv::Point2f> l1, std::list<cv::Point2f> l2) {
-	return l1.front() == l2.front();
+bool listPointsEqual(std::list<cv::Point2f> l1,
+        std::list<cv::Point2f> l2) {
+    return l1.front() == l2.front();
 }
 
 FeatureExtractor::FeatureExtractor() {
-	// TODO Auto-generated constructor stub
+    // TODO Auto-generated constructor stub
 
 }
 
 FeatureExtractor::~FeatureExtractor() {
-	// TODO Auto-generated destructor stub
+    // TODO Auto-generated destructor stub
 }
 
 void FeatureExtractor::refillFeatures(const cv::Mat& oldFrame,
-		std::vector<std::list<cv::Point2f> >& features,
-		const unsigned int& maxFeatures) {
-	std::vector<std::list<cv::Point2f> > result(features);
+        std::vector<std::list<cv::Point2f> >& features,
+        const unsigned int& maxFeatures) {
+    std::vector<std::list<cv::Point2f> > result(features);
 
-	if (features.size() > maxFeatures) {
-		features.resize(maxFeatures);
-	} else if (features.size() < maxFeatures) {
-		std::vector<cv::Point2f> oldFeatures;
-		for (unsigned int i = 0; i < features.size(); ++i) {
-			oldFeatures.push_back(features[i].front());
-		}
-		std::vector<std::list<cv::Point2f> > additionalFeatureList;
-		std::vector<cv::Point2f> additionalFeatures = extractFeatures(oldFrame,
-				maxFeatures - oldFeatures.size());
-		if (additionalFeatures.empty()) {
-			cv::Exception ex(NO_FEATURES_FOUND, "no features found", __func__,
-					__FILE__, __LINE__);
-			throw ex;
-		}
-		additionalFeatureList.resize(additionalFeatures.size());
-		for (unsigned int i = 0; i < additionalFeatures.size(); ++i) {
-			additionalFeatureList[i].push_front(additionalFeatures[i]);
-		}
-		features.insert(features.end(), additionalFeatureList.begin(),
-				additionalFeatureList.end());
-		std::sort(features.begin(), features.end(), compareListPoints);
-		features.erase(
-				std::unique(features.begin(), features.end(), listPointsEqual));
+    if (features.size() > maxFeatures) {
+        features.resize(maxFeatures);
+    } else if (features.size() < maxFeatures) {
+        std::vector<cv::Point2f> oldFeatures;
+        for (unsigned int i = 0; i < features.size(); ++i) {
+            oldFeatures.push_back(features[i].front());
+        }
+        std::vector<std::list<cv::Point2f> > additionalFeatureList;
+        std::vector<cv::Point2f> additionalFeatures = extractFeatures(
+                oldFrame, maxFeatures - oldFeatures.size());
+        if (additionalFeatures.empty()) {
+            cv::Exception ex(NO_FEATURES_FOUND, "no features found",
+                    __func__, __FILE__, __LINE__);
+            throw ex;
+        }
+        additionalFeatureList.resize(additionalFeatures.size());
+        for (unsigned int i = 0; i < additionalFeatures.size(); ++i) {
+            additionalFeatureList[i].push_front(
+                    additionalFeatures[i]);
+        }
+        features.insert(features.end(), additionalFeatureList.begin(),
+                additionalFeatureList.end());
+        std::sort(features.begin(), features.end(),
+                compareListPoints);
+        features.erase(
+                std::unique(features.begin(), features.end(),
+                        listPointsEqual));
 
-	}
+    }
 }
