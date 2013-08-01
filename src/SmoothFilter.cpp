bf21940f546d981d1ce96067b77a64657ed8d47a
Michał Orynicz <morynicz@gmail.com>
2013-08-01 19:22:37 +0200

    Code cleanup and formatting in SmoothFilter.cpp

--- src/SmoothFilter.cpp
+++ src/SmoothFilter.cpp
@@ -9,9 +9,6 @@
 #include <cmath>
 #include <algorithm>
 
-//#include<iostream>
-using namespace std;
-
 SmoothFilter::SmoothFilter(const double& maxDeviation,
         const double& deviantThreshold, const double &minLength,
         const double &maxLength) :
@@ -44,10 +41,6 @@ std::vector<std::list<cv::Point2f> > SmoothFilter::filterFeatures(
         } else {
             std::list<cv::Point2f>::const_iterator it =
                     features[i].begin();
-//			cerr<<"pre"<<endl;
-//            cv::Point2f first = *it;
-//            cv::Point2f second = *(++it);
-//            cv::Point2f third = *(++it);
             cv::Point2f zeroth = *it;
             cv::Point2f first = *(++it);
             ++it;
@@ -56,71 +49,38 @@ std::vector<std::list<cv::Point2f> > SmoothFilter::filterFeatures(
             ++it;
             ++it;
             cv::Point2f seventh = *(++it);
-//			cv::Vec2f v1 = third - second;
-//			cv::Vec2f v2 = second - first;
-//			cerr<<"in "<<acos(
-//                    (v1[0] * v2[0] + v1[1] * v2[1])
-//                            / (cv::norm(v1) + cv::norm(v2)))/CV_PI<<endl;
-
-//            double a = cv::norm(second - third);
-//            double b = cv::norm(first - second);
-//            double c = cv::norm(third - first);
-
-//			cerr<<"in "<<acos((b*b+c*c-a*a)/(2*b*c))/CV_PI;
 
             cv::Vec2f v10 = first - zeroth;
             cv::Vec2f v31 = third - first;
             cv::Vec2f v73 = seventh - third;
 
             double cos1031 = v10.dot(v31)
-                    / (cv::norm(v10) * cv::norm(v31)) * 180/CV_PI;
+                    / (cv::norm(v10) * cv::norm(v31)) * 180 / CV_PI;
             double cos3173 = v31.dot(v73)
-                    / (cv::norm(v31) * cv::norm(v73)) * 180/CV_PI;
+                    / (cv::norm(v31) * cv::norm(v73)) * 180 / CV_PI;
             double cos1073 = v10.dot(v73)
-                    / (cv::norm(v10) * cv::norm(v73)) * 180/CV_PI;
+                    / (cv::norm(v10) * cv::norm(v73)) * 180 / CV_PI;
 
-//            std::cerr << zeroth << std::endl;
-//            std::cerr << first << std::endl;
-//            std::cerr << third << std::endl;
-//            std::cerr << seventh << std::endl;
-//
-//            std::cerr<<cv::norm(v10 )<<std::endl;
-//            std::cerr<<cv::norm(v31 )<<std::endl;
-//            std::cerr<<cv::norm(v73 )<<std::endl;
-//
-//            std::cerr << cos1031 << std::endl;
-//            std::cerr << cos3173 << std::endl;
-//            std::cerr << cos1073 << std::endl;
-//            if (a > _minLength && b > _minLength
-//                    && (_maxDeviation
-//                            < acos(
-//                                    (b * b + c * c - a * a)
-//                                            / (2 * b * c))
-//                            || _maxLength < a + b)) {
-            if (_maxDeviation < acos(abs(cos1031)) || _maxDeviation < acos(abs(cos1073))
+            if (_maxDeviation < acos(abs(cos1031))
+                    || _maxDeviation < acos(abs(cos1073))
                     || _maxDeviation < acos(abs(cos3173))) {
                 toKill[i] = true;
                 ++counter;
-//				std::cerr<<"to kill "<<counter<<" "<<i<<std::endl;
             } else {
                 toKill[i] = false;
             }
-//			cerr<<"post"<<endl;
         }
     }
 
     if (_deviantThreshold >= counter / (double) features.size()) {
         for (unsigned int j = 0; j < toKill.size(); ++j) {
             if (toKill[j]) {
-                // cerr << "bug in " << j << endl;
                 result.erase(result.begin() + j);
                 toKill.erase(toKill.begin() + j);
                 --j;
-                //  cerr << "bug out" << endl;
             }
         }
-    }else{
-  //      std::cerr<<"deviant overflow"<<std::endl;
+    } else {
     }
     return result;
 }
